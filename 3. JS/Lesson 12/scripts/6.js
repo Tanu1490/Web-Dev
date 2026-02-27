de59e1d44c08449a6dcc973451885c8a8b7c2ea2
Tanu1490 <tanushree110803@gmail.com>
2026-02-27 06:22:16 +0530

    update changes

--- 3. JS/Lesson 12/scripts/6.js
+++ 3. JS/Lesson 12/scripts/6.js
@@ -2,121 +2,106 @@ let score = JSON.parse(localStorage.getItem("score")) || {
   wins: 0,
   losses: 0,
   ties: 0,
-}; // || default op i.e if 1st part is false save 2nd part to score
-
-// if(score === null){    //shortcut !score means if score is null then !score will be !falsy = true
-//   score = {
-//     wins:0,
-//     losses:0,
-//     ties:0
-//   }
-// }
+};
 
 updatScoreELement();
 
 document.querySelector('.js-autoplay-btn')
-  .addEventListener('click', ()=>{
-     autoPlay();
+  .addEventListener('click', () =>{
+    autoPlay();
   });
 
-
-document.querySelector('.js-reset-btn')
-  .addEventListener('click', () => {
-    let HTML = '';
-    const reset_html = `<p>Are you sure you want to reset the score?
-    <button class = 'js-btn'>Yes</button>
-    <button class = 'js-btn'>No</button>
-    `
-    HTML += reset_html;
-
-    document.querySelector('.js-reset-confirmMsg').innerHTML = HTML;
-
-    document.querySelector('.js-btn').addEventListener('click', ()=>{
-    if(document.querySelector('.js-btn').innerHTML === 'Yes'){
-         score.wins = 0;
+function resetScore(){
+        score.wins = 0;
         score.losses = 0;
         score.ties = 0;
 
         localStorage.removeItem('score');
 
         updatScoreELement();
-    }else{
-     document.querySelector('.js-reset-confirmMsg').innerHTML = '<p></p>';
-    }
-  })
-     
+};
+
+document.querySelector('.js-reset-score-btn')
+  .addEventListener('click', () =>{
+    showResetConfirmation();
+    // resetScore();
   });
 
-  
 
-document.body.addEventListener('keydown' , (event) =>{
-  if(event.key === 'Backspace'){
-      score.wins = 0;
-        score.losses = 0;
-        score.ties = 0;
-
-        localStorage.removeItem('score');
-
-        updatScoreELement();
-  }
-})
 let isAutoPlaying = false;
 let intervalId;
 
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval( () => {
+    intervalId = setInterval(() => {
       const playerMove = pickCompMove();
       playGame(playerMove);
     }, 1000);
     isAutoPlaying = true;
-    document.querySelector('.js-autoplay-btn')
-      .innerHTML = 'Stop Playing';
 
+    document.querySelector('.js-autoplay-btn').innerHTML = 'Stop Playing';
   } else {
     clearInterval(intervalId);
     isAutoPlaying = false;
-    document.querySelector('.js-autoplay-btn')
-    .innerHTML = 'Auto Play';
+    document.querySelector('.js-autoplay-btn').innerHTML = 'Auto Play';
   }
-
-
-
-
-
-
 }
-//  .addEventListener('click', playGame('rock'));  - playGame('rock') this will run the function and here we need to give function 
 
-document.querySelector('.js-Rock-btn')
- .addEventListener('click', () => {
-    playGame('Rock');
- });
+document.querySelector(".js-Rock-btn").addEventListener("click", () => {
+  playGame("Rock");
+});
 
+document.querySelector(".js-Paper-btn").addEventListener("click", () => {
+  playGame("Paper");
+});
 
- document.querySelector('.js-Paper-btn')
- .addEventListener('click', () => {
-    playGame('Paper');
- });
+document.querySelector(".js-Scissors-btn").addEventListener("click", () => {
+  playGame("Scissors");
+});
 
- 
- document.querySelector('.js-Scissors-btn')
- .addEventListener('click', () => {
-    playGame('Scissors');
- });
-
- document.body.addEventListener('keydown', (event) => {
-  if(event.key === 'r'){
-    playGame('Rock');
-  }else if(event.key === 'p'){
-     playGame('Paper');
-  }else if(event.key === 's'){
-     playGame('Scissors');
+document.body.addEventListener("keydown", (event) => {
+  console.log("key");
+  if (event.key === "r") {
+    playGame("Rock");
+  } else if (event.key === "p") {
+    playGame("Paper");
+  } else if (event.key === "s") {
+    playGame("Scissors");
   }else if(event.key === 'a'){
-     autoPlay();
+    autoPlay();
+  }else if(event.key === 'Backspace'){
+    resetScore();
   }
- })
+});
 
+
+function showResetConfirmation(){
+  document.querySelector('.js-reset-confirmation')
+    .innerHTML = `Are you sure you want to reset the score?
+      <button class="js-reset-confirm-yes reset-confirm-button">
+        Yes
+      </button>
+      <button class="js-reset-confirm-no reset-confirm-button">
+        No
+      </button>`;
+
+
+      document.querySelector('.js-reset-confirm-yes')
+       .addEventListener('click',() => {
+        resetScore();
+         hideResetConfirmation();
+});
+
+ document.querySelector('.js-reset-confirm-no')
+       .addEventListener('click',() => {
+         hideResetConfirmation();
+});
+}
+
+function  hideResetConfirmation(){
+   document.querySelector('.js-reset-confirmation')
+    .innerHTML ='';
+}
 
 function playGame(playerMove) {
   const CompMove = pickCompMove();
@@ -139,8 +124,6 @@ function playGame(playerMove) {
   if (result === "You win") score.wins++;
   else if (result === "You lose") score.losses++;
   else if (result === "Tie") score.ties++;
-
-  // localStorage.setItem('message', 'hello'); //this will save hello to message in localstorage
 
   localStorage.setItem("score", JSON.stringify(score));
   updatScoreELement();
